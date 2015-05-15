d00378a91603f79ba557b92cc69f99a950c4d20b
MonCherWatson <jose.marie.rodriguez@gmail.com>
2015-05-15 16:14:57 +0200

    spring boot

--- service/src/main/java/com/auguryrock/luv4s/Luv4sApplication.java
+++ service/src/main/java/com/auguryrock/luv4s/Luv4sApplication.java
@@ -2,10 +2,14 @@ package com.auguryrock.luv4s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @Configuration
+@ComponentScan(basePackages = {"com.auguryrock.luv4s"})
+@ImportResource({"/applicationContext-rest.xml", "/applicationContext-derby.xml"})
 public class Luv4sApplication {
 
 }
